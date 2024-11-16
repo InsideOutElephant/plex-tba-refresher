1d3d82143a16bd73f6f70e0b31cfad70800de515
Neil McNally <neil@stellamarisnursinghome.ie>
2024-11-16 18:55:32 +0000

    fix token issues plexserver instance from plexserver library was expecting xml, however we were requesting json from the plex server. changed to requesting xml

--- plex_refresher/core/plex_client.py
+++ plex_refresher/core/plex_client.py
@@ -12,7 +12,7 @@ class PlexClient:
         
         # Log token format for debugging
         self.logger.debug(f"Token length: {len(self.token)}")
-        self.logger.debug(f"Token preview: {self.token[:4]}...{self.token[-4:] if len(self.token) >= 8 else ''}" // FAILING TO CONNECT TO SERVER DUE TO INVALID TOKEN. THESE LOGGER MESSAGES ARE NOT APPEARING IN THE OUTPUT. NOT SURE WHERE THE CONNECTION IS OCCURRING IF NOT HERE
+        self.logger.debug(f"Token preview: {self.token[:4]}...{self.token[-4:] if len(self.token) >= 8 else ''}") # // FAILING TO CONNECT TO SERVER DUE TO INVALID TOKEN. THESE LOGGER MESSAGES ARE NOT APPEARING IN THE OUTPUT. NOT SURE WHERE THE CONNECTION IS OCCURRING IF NOT HERE
         self.logger.debug(f"Token contains whitespace: {' ' in self.token}")
         self.logger.debug(f"Token is alphanumeric: {self.token.isalnum()}")
 
@@ -28,19 +28,11 @@ class PlexClient:
                 session = requests.Session()
                 session.headers.update({
                     'X-Plex-Token': self.token,
-                    'Accept': 'application/json',
+                    'Accept': 'application/xml',
                     'X-Plex-Client-Identifier': 'plex-tba-refresher',
                     'X-Plex-Product': 'Plex TBA Refresher',
                     'X-Plex-Version': '1.0'
                 })
-
-                # Try a basic request first to validate token
-                test_url = f"{self.url}/?X-Plex-Token={self.token}"
-                response = session.get(test_url)
-                self.logger.debug(f"Test request status code: {response.status_code}")
-                
-                if response.status_code != 200:
-                    raise Exception(f"Failed to validate token. Status code: {response.status_code}")
 
                 # Now create the PlexServer instance
                 plex = PlexServer(
